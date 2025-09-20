e1040169e7305fbd5dd88a0c0aa366cecbf9bfbe
San7o <santigio2003@gmail.com>
2025-09-20 11:13:26 +0200

    hashmap.h: initial commit

---
+++ Makefile
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: MIT
+
+## --- Settings ---
+
+CFLAGS=-Wall -Werror -Wpedantic -ggdb -std=c99 -Wno-unused-function
+LDFLAGS=
+CC=gcc
+
+OUT_NAME=example
+OBJ=example.o
+
+## --- Commands ---
+
+# --- Targets ---
+
+all: $(OUT_NAME)
+
+run: $(OUT_NAME)
+	chmod +x $(OUT_NAME)
+	./$(OUT_NAME)
+
+$(OUT_NAME): $(OBJ)
+	$(CC) $(OBJ) $(LDFLAGS) $(CLAGS) -o $(OUT_NAME)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm $(OBJ) 2>/dev/null || :
+
+distclean:
+	rm $(OUT_NAME) 2>/dev/null || :
---
+++ example.c
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+#define HASHMAP_IMPLEMENTATION
+#include "hashmap.h"
+
+#include <stdio.h>
+#include <assert.h>
+
+// A custom type
+typedef struct {
+  int a;
+  char* b;
+} ExampleType;
+
+// Define deep copy and free functions
+ExampleType example_deep_copy(ExampleType *type) {
+  printf("Deep copy\n"); return *type;
+}
+void example_free(ExampleType *type) {
+  printf("Free\n"); return;
+}
+
+// Declare the hashmap
+HASHMAP_DECLARE(example,
+                hashmap_hash_char,
+                char*,
+                ExampleType,
+                example_deep_copy,
+                example_free)
+
+int main(void)
+{
+  example_map map;
+  assert(example_map_init(&map, 1024) == 0);
+
+  ExampleType example = {
+    .a = 10,
+    .b = "it just works",
+  };
+  assert(example_map_update(&map, "example", 8, &example) == 0);
+
+  ExampleType *value = example_map_lookup(&map, "example", 8);
+  assert(value != NULL);
+  assert(value->a == 10);
+  assert(value->b == example.b);
+
+  example_map *map_copy = example_map_deep_copy(&map);
+  assert(map_copy != NULL);
+  
+  ExampleType *value_copy = example_map_lookup(map_copy, "example", 8);
+  assert(value_copy != NULL);
+  assert(value_copy->a == 10);
+  assert(value_copy->b == example.b);
+
+  
+  assert(example_map_destroy(&map) == 0);
+  assert(example_map_destroy(map_copy) == 0);
+  return 0;
+}
